771f138ddd4a56a83f967bfd530e6a8ee71174a8
lukaszslaweta <lukasz.slaweta@gmail.com>
2022-03-18 17:47:34 +0100

    add assets and views

--- Quizzy/ViewController.swift
+++ Quizzy/ViewController.swift
@@ -9,10 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+    }
+    
 
 
 }
